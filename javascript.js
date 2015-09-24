fcd6c096220e33b7fdce5b308dc9fe1bf09c0e21
Eric Leung <etleung21@gmail.com>
2015-09-24 01:37:30 -0700

    add notes on js numbers and arrays

--- javascript.js
+++ javascript.js
@@ -5,3 +5,16 @@ var name = "Joe";
 name.length; // get length of string
 name[0]; // get "first" element of string
 name[name.length - 1]; // get last element of string
+
+// numbers
+var number = 4; // set number
+var other = (4 * 2) + 4; // combining operations
+
+// arrays
+var sampleArray = ["Joe", 42]; // holds different data
+var nested = [["Bob", 32]]; // make nested array
+sampleArray[0]; // access first element, zero-indexed
+sampleArray.pop(); // remove and return last element
+sampleArray.push(42); // "push" data to last element
+sampleArray.shift(); // remove first element
+sampleArray.unshift("Joe"); // add data to first element
