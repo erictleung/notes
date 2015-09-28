9808c660f8db67c54ce53e30f944e0c2d163ed65
leunge <leunge@ohsu.edu>
2015-09-28 15:21:59 -0700

    add git notes on squashing commits together

--- git.bash
+++ git.bash
@@ -6,3 +6,6 @@ git rm --cached <filename> # stop tracking file
 # dealing with remotes
 git remote -v # looking at all remotes
 git remote rm <remote_name> # remove remote
+
+# squash commit with last commit and use new message
+git commit --amend -m "new message here"
