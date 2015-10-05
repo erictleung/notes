5782fcce84ad04f4fcce5f75109fdcb4e5e5389a
Eric Leung <etleung21@gmail.com>
2015-10-04 22:51:39 -0700

    add increment and decrement for Scala

--- scala.scala
+++ scala.scala
@@ -4,3 +4,6 @@ val num1 = 10 // declaring value
 var num2 = 20 // declaring variable
 
 1.to(10) // yields Range(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+num2 += 5 // increment by 5
+num2 -= 5 // decrement by 5
