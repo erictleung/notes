06292c4219d272215b9c0fb941db413aac32a21b
Eric Leung <etleung21@gmail.com>
2015-10-07 23:27:41 -0700

    add timeout command

--- bash.bash
+++ bash.bash
@@ -5,3 +5,6 @@ cd -
 
 # open new instance of application
 open -n -a Application
+
+# time out commands if they take too long (command doesn't natively exist on MacOS)
+timeout 5 top # time out after 5 seconds
