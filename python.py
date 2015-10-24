1454517b517bf5471a62754db51191a5c3b6a1f3
Eric Leung <etleung21@gmail.com>
2015-10-24 16:19:13 -0700

    add how to read file in Python

--- python.py
+++ python.py
@@ -8,3 +8,8 @@ for i in range(10):
 randList = ["a", "ab", "bc", "de", "abc"]
 toFind = "a"
 print [x for x in randList if toFind in x]
+
+# read file
+with open("filename.txt", "r") as fh:
+    data = fh.readline() # read line by line
+    # data = fh.read() # read entire file
