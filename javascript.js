bf806587af815cb0c269197f09d463d2d7264cc6
Eric Leung <etleung21@gmail.com>
2015-11-08 16:20:25 -0800

    add js notes on map, reduce, and filter methods on arrays

--- javascript.js
+++ javascript.js
@@ -12,6 +12,7 @@ var other = (4 * 2) + 4; // combining operations
 Math.random(); // creates floating point num 0 (incl) to 1 (excl)
 
 // arrays
+var numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // number array
 var sampleArray = ["Joe", 42]; // holds different data
 var nested = [["Bob", 32]]; // make nested array
 sampleArray[0]; // access first element, zero-indexed
@@ -19,6 +20,21 @@ sampleArray.pop(); // remove and return last element
 sampleArray.push(42); // "push" data to last element
 sampleArray.shift(); // remove first element
 sampleArray.unshift("Joe"); // add data to first element
+
+// map method on array - apply method over all elements
+var timesFour = numArray.map(function(val) {
+  return val * 4;
+})
+
+// reduce method on array - condense elements to one value
+var addAll = numArray.reduce(function(pre, curr) {
+  return pre + curr;
+}, 0);
+
+// filter method on array - filter elements of array based on condition
+var lessSix = numArray.filter(function(val) {
+  return val < 5;
+});
 
 // functions
 function ourFunction(a, b) {
