e4e4ba5174d459fadea4fabbafdd3d5505ec9a93
Eric Leung <leunge@ohsu.edu>
2015-11-18 10:24:28 -0800

    create alphabet list of bash commands commonly used

---
+++ alphabet.bash
@@ -0,0 +1,111 @@
+# Alphabet of Bash Commands
+
+## A
+alias
+awk
+
+## B
+bzip2
+
+## C
+cat
+cd
+chmod
+chown
+clear
+cp
+
+## D
+df
+
+## E
+echo
+export
+
+## F
+
+
+## G
+grep
+gzip
+
+## H
+head
+history
+htop
+
+## I
+
+
+## J
+
+
+## K
+
+
+## L
+less
+ln
+ls
+
+## M
+make
+man
+mkdir
+more
+mv
+
+## N
+
+
+## O
+
+
+## P
+ping
+pwd
+
+## Q
+
+
+## R
+rm
+rmdir
+
+## S
+scp
+sed
+sleep
+sort
+source
+ssh
+su
+sudo
+
+## T
+tail
+tar
+time
+touch
+top
+
+## U
+unalias
+uniq
+
+## V
+vim
+
+## W
+wc
+whoami
+wget
+
+## X
+
+
+## Y
+
+
+## Z
+zcat
