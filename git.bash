b66ea1929184f9289448d5fa3a290d35e2d64cce
Eric Leung <leunge@ohsu.edu>
2015-11-19 22:56:47 -0800

    add git notes on saving ID and update filename placeholders

--- git.bash
+++ git.bash
@@ -1,11 +1,15 @@
 # git Notes
 
+# save your identity
+git config --global user.name "John Doe"
+git config --global user.email johndoe@example.com
+
 # removing files
-git rm --cached <filename> # stop tracking file
+git rm --cached example.txt # stop tracking file
 
 # dealing with remotes
 git remote -v # looking at all remotes
-git remote rm <remote_name> # remove remote
+git remote rm example-remote # remove remote
 
 # squash commit with last commit and use new message
 git commit --amend -m "new message here"
