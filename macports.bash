8248f294492c24c6a263322a0cee7dc2652f0f8e
Eric Leung <etleung21@gmail.com>
2015-12-29 00:33:17 -0800

    Start Macport notes

---
+++ macports.bash
@@ -0,0 +1,10 @@
+# Macport Commands
+
+# remove inactive/old package versions
+sudo port uninstall inactive
+
+# update Macports itself
+sudo port selfupdate
+
+# update outdated programs
+sudo port upgrade outdated
