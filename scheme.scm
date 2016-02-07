4dbe45f7a5b2174e624878b6592a36ef416683c5
Eric Leung <eric@erictleung.com>
2016-02-07 13:39:41 -0800

    scheme.scm: start Scheme notes

---
+++ scheme.scm
@@ -0,0 +1,17 @@
+; Scheme Notes
+
+; one kind of interpreter
+csi ; chicken interpreter
+
+; compile file with interpreter
+csc hello.scm ; chicken compiler
+
+; simple arithmetic
+(+ 1 2 3 4) ; 10
+(- 1 2 3 4) ; 8
+
+; set variable pi
+(define pi 3.14159)
+
+set compound procedure/function
+(define (square x) (* x x))
