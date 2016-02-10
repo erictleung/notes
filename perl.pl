ad5bbf085e1399b427fb90c8b77872eb67183596
Eric Leung <eric@erictleung.com>
2016-02-10 10:02:11 -0800

    perl: update and add notes on 2-D array
    - Remove extra spaces
    - Use last index of an array
    - Access element in 2-dimensional array
    - Print sequence of numbers
    - Loop through 2-dimensional array

--- perl.pl
+++ perl.pl
@@ -10,7 +10,7 @@ close FILE1;
 # append to file
 open ( FILE2, "> out.txt" );
 my $name = "Fred";
-print FILE2 $name; 
+print FILE2 $name;
 close FILE2;
 
 # functions/subroutines
@@ -25,14 +25,29 @@ $a[0] = 1;
 $a[1] = 2;
 $a[2] = 4;
 
+# return last index of the array
+print $#array1, "\n";
+
 # two-dimensional array
 my @two = (
     ["a", "b", "c"],
     ["d", "e", "f"],
     ["g", "h", "i"]
 );
+print $two[1][2], "\n"; # "f"
 
 # conditional statement
 if ($a[2] == 0) {
     print "Matched!\n";
+}
+
+# create sequence of numbers
+print 0..3, "\n"; 0123
+
+# loop through two dimensional array
+foreach my $first (@two) { # loop through first dimension of array
+    print "Each row has: ";
+    foreach (@$ref) { # goes through row itself
+        print "$_ ";
+    }
 }
