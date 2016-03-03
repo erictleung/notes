23c1cc6d006575bd51aeccfea12b5cc34aea492d
Eric Leung <eric@erictleung.com>
2016-03-02 16:24:25 -0800

    pandoc: note compile LaTeX with table of contents

--- pandoc.bash
+++ pandoc.bash
@@ -2,3 +2,7 @@
 
 # make source code have nice shaded background
 pandoc example.md -o example.pdf --highlight-style=tango
+
+# compile document with table of contents
+pandoc example.md -o example.pdf --toc # or...
+pandoc example.md -o example.pdf --table-of-contents
