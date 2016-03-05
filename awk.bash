dc16615db780a1e5c419cfa4b21eb8ce5a42a431
Eric Leung <eric@erictleung.com>
2016-03-04 21:29:32 -0800

    awk: add general notes on how awk works

--- awk.bash
+++ awk.bash
@@ -1,5 +1,22 @@
 # awk Notes
 
+# commands follow form of:
+#   pattern { action }
+
+# or they follow the form of:
+#   BEGIN { print "START" }  # thing to do before anything
+#         { print         }  # thing to do while reading lines
+#   END   { print "STOP"  }  # thing to do after reading everything
+
+# awk doesn't interpret variables within strings
+# e.g. { print "$8\t$3" } doesn't work
+
+# you can specify variables (e.g. will print number "5" and fifth field)
+awk '
+BEGIN   { x=5 }         \
+        { print x, $5 } \
+'
+
 # print entire file
 awk '{ print }' example.txt
 awk '{ print $0 }' example.txt
