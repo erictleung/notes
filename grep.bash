7816a5980acd24130a4a1e63f96833db41fe9593
Eric Leung <eric@erictleung.com>
2016-05-17 00:50:23 -0700

    grep: search all files, one file, or not in file

--- grep.bash
+++ grep.bash
@@ -1,6 +1,16 @@
 # grep Notes
 
+# search for word within files in currect directory
+grep hello *
+
 # recursively search all files for a string
 cd /path/you/want
 grep -r "word" .
 grep -ir "WoRd" .  # ignores case
+
+# grep is suppress file name if given one file
+grep grep grep.bash
+grep grep grep.bash /dev/null  # if you want the file name
+
+# show lines not showing pattern
+grep -v bash grep.bash
