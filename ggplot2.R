04e30f82cb5d59fb054c8d5a10164fd8faef18c0
Eric Leung <eric@erictleung.com>
2016-06-19 22:19:51 -0700

    ggplot2: change legend title manually

--- ggplot2.R
+++ ggplot2.R
@@ -7,3 +7,11 @@ require(gridExtra)
 plot1 <- qplot(rnorm(100), binwidth = 0.5)
 plot2 <- qplot(rpois(100, 3), binwidth = 0.5)
 grid.arrange(plot1, plot2, ncol = 2)
+
+# change legend title
+# Source: http://stackoverflow.com/a/14622513
+p <- ggplot(df, aes(x=rating, fill=cond)) +
+            geom_density(alpha=.3) +
+            xlab("NEW RATING TITLE") +
+            ylab("NEW DENSITY TITLE")
+p <- p + guides(fill=guide_legend(title="New Legend Title"))
