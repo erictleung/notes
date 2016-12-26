3fce7017b6f0922f260342ce9ae408dd7e3144cd
Eric Leung <eric@erictleung.com>
2016-12-26 14:40:02 -0800

    macports: show outdated and inactive ports + rm

--- macports.bash
+++ macports.bash
@@ -8,3 +8,12 @@ sudo port selfupdate
 
 # update outdated programs
 sudo port upgrade outdated
+
+# show ports which need updating
+port outdated
+
+# show inactive ports
+port installed inactive
+
+# remove inactive ports
+sudo port uninstall inactive
