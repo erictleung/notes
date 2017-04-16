181bd189a3b7820cff244fe8aad2141143b2190b
Eric Leung <eric@erictleung.com>
2017-04-16 12:03:25 -0700

    cpp: start C++ notes

---
+++ cpp.cpp
@@ -0,0 +1,17 @@
+// C++ Notes
+
+// Compile using c++
+// > g++ -o output input.cpp
+// source: http://ce.uml.edu/compile.htm
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello, World!\n";
+  cin.get();
+
+  return 1;
+}
