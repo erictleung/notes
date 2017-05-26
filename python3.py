3f84a1b07a2f5e471ca46a0ef605558b6a6d5c44
Eric Leung <eric@erictleung.com>
2017-05-25 18:58:19 -0700

    python3: add various notes for Python3
    - Basic data types
    - Multiple assignment
    - Autoconcatenation of strings
    - Range generator
    - Defining functions

--- python3.py
+++ python3.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
+# data types in Python3
+2       # integers and numbers
+"eggs"  # strings
+[1, 2]  # lists
+
+# multiple assignment
+a, b = 0, 1
+
 # reload local or any modules
 # Source: http://stackoverflow.com/a/14390676
 import imp
 imp.reload(module)
+
+# autoconcatenate strings, doesn't work with variables
+'Py' 'thon' # first way
+('Py'
+ 'thon') # another way
+
+# get range generator
+range(5)        # returns 'range(0, 5)'
+range(2, 5)     # returns 'range(2, 5)'
+list(range(5))  # get contents out
+
+# function definition
+def add_two(a, b):
+    return(a + b)
