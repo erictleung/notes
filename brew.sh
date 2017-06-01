26d8c58d9a117ce42f07dae74bd8fc8140ff56ad
Eric Leung <eric@erictleung.com>
2017-05-31 17:41:36 -0700

    brew: start homebrew notes with OpenSSL error

---
+++ brew.sh
@@ -0,0 +1,5 @@
+# Homebrew Notes
+
+# Failed setting certificate location verification
+#   "Download failed: https://curl.haxx.se/ca/cacert-2017-01-18.pem"
+#   cp cacert-2017-01-18.pem ~/.linuxbrew/etc/openssl/cert.pem
