e18bb6c915be74883dd4c3a61195b84beb27cc9e
Eric Leung <eric@erictleung.com>
2018-10-23 11:42:04 -0700

    brew: remove package caches and old versions

--- brew.sh
+++ brew.sh
@@ -3,3 +3,7 @@
 # Failed setting certificate location verification
 #   "Download failed: https://curl.haxx.se/ca/cacert-2017-01-18.pem"
 #   cp cacert-2017-01-18.pem ~/.linuxbrew/etc/openssl/cert.pem
+
+# Clean up cache to save space
+brew cleanup
+brew cleanup -n # See what will be removed
